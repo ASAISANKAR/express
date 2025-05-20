37706cace89026f0a4ec8bc58ab0d57feb01596a
A Sai Sankar <saisankar3193@gmail.com>
2025-05-21 00:47:32 +0530

    update

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ app.get('/',(req,res)=>{
     app.set('trust proxy', true)
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     res.send({status:"ok",response:"Hello World",host:req.get('host'),ip:ip})
+    console.log(ip)
 })
 app.listen("3000")
