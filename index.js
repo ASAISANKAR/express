242e11f526400917903e7e7e7cc1c1a1a697c128
A Sai Sankar <saisankar3193@gmail.com>
2025-05-21 00:24:13 +0530

    update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let app = express()
 
 app.get('/',(req,res)=>{
     console.log(req.get('host'))
-    res.send({status:"ok",response:"Hello World"})
+    res.send({status:"ok",response:"Hello World",host:req.get('host')})
 })
 app.listen("3000")
