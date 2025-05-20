bad225232dd5b8ea3edfbb131b8e79ef14969d41
A Sai Sankar <saisankar3193@gmail.com>
2025-05-21 00:36:45 +0530

    added IP

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let app = express()
 
 app.get('/',(req,res)=>{
     console.log(req.get('host'))
+    console.log(req.ip)
     res.send({status:"ok",response:"Hello World",host:req.get('host')})
 })
 app.listen("3000")
